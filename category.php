e63d213a71f588066bb5a32e1ecc12f0f0dc47f7
sach-007 <sachinbachhav600@gmail.com>
2022-04-02 14:28:32 +0530

    fourth post commit

--- category.php
+++ category.php
@@ -16,7 +16,12 @@
 
 
                 <?php
-                $query = "SELECT * FROM posts";
+
+                if (isset($_GET['category'])) {
+                    $post_category_id = $_GET['category'];
+                }
+
+                $query = "SELECT * FROM posts WHERE post_category_id = $post_category_id ";
                 $select_all_posts_query = mysqli_query($connection,$query);
 
                         while($row = mysqli_fetch_assoc($select_all_posts_query))
@@ -26,7 +31,9 @@
                             $post_author =  $row['post_author'];
                             $post_date =  $row['post_date'];
                             $post_image =  $row['post_image'];
+                            
                             $post_content =  substr($row['post_content'],0,100);
+
                             // echo "<li><a href='#'> $post_title </a></li>";
                             // $post_title
 
